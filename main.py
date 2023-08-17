ffc0185c7fd204d82ad1c6cc16d3bf4e4693bce6
bigvik <bigvikt@gmail.com>
2023-08-17 22:04:49 +0600

    Now with listbox

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import Frame, BOTH, X, N, LEFT, RIGHT
 from abc import ABC, abstractmethod
 import threading
@@ -36,6 +37,9 @@ class App(tk.Tk):
         self.frame1 = Frame(self.frame)
         self.frame1.pack(fill=X)
 
+        self.frame2 = Frame(self.frame)
+        self.frame2.pack(fill=X)
+
         self.label_brutto = tk.Label(self.frame1, text='Brutto:', width=10)
         self.label_brutto.pack(side=LEFT, padx=5, pady=5)
 
@@ -45,8 +49,15 @@ class App(tk.Tk):
         self.label_netto = tk.Label(self.frame1, text='Netto:', width=10)
         self.label_netto.pack(side=LEFT, padx=5, pady=5)
 
-        self.button_add = tk.Button(self.frame1, text = 'Измерить', command=self.get_weight)
+        self.button_add = tk.Button(self.frame1, text = 'Тестировать', command=self.get_weight)
         self.button_add.pack(side=LEFT, padx=5, pady=5)
+
+        cols = ('Date', 'Brutto', 'Netto', 'Tara', 'Dist')
+        self.listBox = ttk.Treeview(self.frame2, columns=cols, show='headings')
+        # set column headings
+        for col in cols:
+            self.listBox.heading(col, text=col)    
+        self.listBox.grid(row=1, column=0, columnspan=2)
 
     def switch_button_state(self):
         if (self.button_add['state'] == tk.NORMAL): self.button_add['state'] = tk.DISABLED
@@ -62,6 +73,7 @@ class App(tk.Tk):
         self.label_brutto.config(text = 'Brutto: ' + str(data[1]))
         self.label_tara.config(text = 'Tara: ' + str(data[2]))
         self.label_netto.config(text = 'Netto: ' + str(data[3]))
+        self.listBox.insert("", "end", values=(data))
 
 
 
