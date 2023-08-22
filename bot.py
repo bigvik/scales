093f978768f27d54b3f2f279d8252f8cb186d7b6
bigvik <bigvikt@gmail.com>
2023-08-23 01:01:25 +0600

    Work on bot

--- bot.py
+++ bot.py
@@ -1,19 +1,24 @@
-import asyncio
+import telebot
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
 from abc import ABC, abstractmethod
-import threading
-import time
 
 import control
 import config
 import test
 
-bot = Bot(config.BOT_TOKEN)
-dp = Dispatcher()
-chat_id = 0
-buffer = []
+bot = telebot.TeleBot(config.BOT_TOKEN)
+cid = 0
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    cid = message.chat.id
+    bot.reply_to(message, f"Howdy, how are you doing {cid}?")
+    bot.send_message(cid,"Привет ✌️ ")
+        
+def send_msg(sub):
+     bot.send_message(chat_id=cid, text=sub)
+
 
 class BotObserver(ABC):
     '''Абстрактный слушатель'''
@@ -24,45 +29,16 @@ class BotObserver(ABC):
         pass
 
 
-def msg(subject):
-    buffer.append(subject)
-
 class BotListener(BotObserver):
     '''Конкретный слушатель'''
 
     def update(self, subject):
-        msg(subject)
+        send_msg(subject)
 
-logging.basicConfig(level=logging.INFO)
-
-
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    chat_id = message.chat.id
-    await message.answer(f"Hello! Chat_Id: {chat_id}")
-    test.test_bot()
-    #test()
-
-
-# async def process_messages(queue):
-#     while True:
-#         message = await queue.get()
-#         await bot.send_message(chat_id, message)
-
-
-""" def test():
-    for x in range(10):
-        bot.send_message(chat_id, x)
-        time.sleep(3) """
-
-async def main():
-    # queue = asyncio.Queue()
-    # asyncio.ensure_future(process_messages(queue))
-    #await asyncio.sleep(1)
-    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     
     lister = BotListener()
     control.anons.attach(lister)
-    asyncio.run(main())
+
+    bot.infinity_polling()
